63ac7def0b1e6a11a4a581cf0cf5a600b0090227
Devi Handevi <devihandevi@gmail.com>
2020-12-03 15:36:25 +0700

    [Room Search] Models and view for Hotel, Room, RoomType, and Price

--- bobobox_hotel/api/admin.py
+++ bobobox_hotel/api/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Hotel, RoomType, Room, Price
+
+admin.site.site_header = "Bobobox Admin"
+admin.site.site_title = "Bobobox Admin Area"
+admin.site.index_title = "Welcome to Bobobox admin area"
+
+
+class HotelAdmin(admin.ModelAdmin):
+  list_display = ('hotel_name', 'address',)
+  fields = ('hotel_name', 'address',)
+
+
+class RoomTypeAdmin(admin.ModelAdmin):
+  fields = ('name',)
+
+
+class RoomAdmin(admin.ModelAdmin):
+  list_display = ('hotel_id', 'room_type_id', 'room_status', 'room_number',)
+  fields = ('hotel_id', 'room_type_id', 'room_status', 'room_number',)
+
+
+class PriceAdmin(admin.ModelAdmin):
+  list_display = ('room_type_id', 'price', 'date',)
+  fields = ('room_type_id', 'price', 'date',)
+
+
+admin.site.register(Hotel, HotelAdmin)
+admin.site.register(RoomType, RoomTypeAdmin)
+admin.site.register(Room, RoomAdmin)
+admin.site.register(Price, PriceAdmin)
--- bobobox_hotel/api/models.py
+++ bobobox_hotel/api/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+class Hotel(models.Model):
+  hotel_name = models.CharField(max_length=50)
+  address = models.CharField(max_length=200)
+
+  def __str__(self):
+    return self.hotel_name
+
+
+class RoomType(models.Model):
+  name = models.CharField(max_length=50)
+
+  def __str__(self):
+    return self.name
+
+
+class Room(models.Model):
+  hotel_id = models.ForeignKey(Hotel, on_delete=models.PROTECT)
+  room_type_id = models.ForeignKey(RoomType, on_delete=models.PROTECT)
+  room_status = models.BooleanField(default=True)
+  room_number = models.IntegerField()
+
+  def __str__(self):
+    return self.hotel_id.hotel_name + ' - ' + self.room_type_id.name + ' - ' + str(self.room_number)
+
+
+class Price(models.Model):
+  room_type_id = models.ForeignKey(RoomType, on_delete=models.PROTECT)
+  price = models.IntegerField()
+  date = models.DateField()
+
+  def __str__(self):
+    return self.room_type_id.name + ' ' + str(self.price)
